83a1d23ac1eb04db909d132fa66446235d076b23
jihyun1029 <parkji2028@naver.com>
2025-07-27 21:45:30 +0900

    회원가입 FE

--- todo-fe/src/App.js
+++ todo-fe/src/App.js
@@ -1,96 +1,19 @@
-import {useEffect, useState} from "react";
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import TodoBoard from "./components/TodoBoard";
-
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import api from './utils/api';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import TodoPage from "./pages/TodoPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-    const [todoList, setTodoList] = useState([]);
-    const [todoValue, setTodoValue] = useState('');
+    return (
+        <Routes>
+            <Route path="/" element={<TodoPage />} />
+            <Route path="/register" element={<RegisterPage />} />
 
-    const getTask = async () => {
-        const response = await api.get('/tasks'); // api 가져오기
-        console.log('rrrr', response);
-        setTodoList(response.data.data);
-    };
-
-    const addTask = async () => {
-        try {
-            const response = await api.post('/tasks', { // 어떤 내용을 생성하고 싶은지도 전달해줘야 한다.
-                task:todoValue,
-                isComplete: false
-            });
-            if (response.status === 200) {
-                console.log('성공')
-                // 1. 입력한 값이 안 사라짐
-                setTodoValue('');
-                // 2. 추가한 값이 안 보임
-                getTask()
-            } else {
-                throw new Error('task can not be added')
-            }
-        } catch (err) {
-            console.log('error', err)
-        }
-    }
-
-    const toggleComplete = async (id) => {
-        try {
-            const task = todoList.find((item) => item._id === id);
-            const response = await api.put(`/tasks/${id}`, {
-                isComplete: !task.isComplete,
-            });
-            if (response.status === 200) {
-                getTask();
-            }
-        } catch (err) {
-            console.log('error', err)
-        }
-    }
-
-    const deleteItem = async (id) => {
-        try {
-            const response = await api.delete(`/tasks/${id}`);
-            if (response.status === 200) {
-                getTask();
-            }
-        } catch (err) {
-            console.log('error', err);
-        }
-    }
-
-    useEffect(() => {
-        getTask()
-    }, []);
-  return (
-    <Container>
-      <Row className="add-item-row">
-        <Col xs={12} sm={10}>
-          <input
-            type="text"
-            placeholder="할일을 입력하세요"
-            className="input-box"
-            value={todoValue}
-            onChange={(event)=>setTodoValue(event.target.value)}
-          />
-        </Col>
-        <Col xs={12} sm={2}>
-          <button className="button-add" onClick={addTask}>추가</button>
-        </Col>
-      </Row>
-
-      <TodoBoard
-          todoList={todoList}
-          deleteItem={deleteItem}
-          toggleComplete={toggleComplete}
-      />
-    </Container>
-  );
+            <Route path="/login" element={<LoginPage />} />
+        </Routes>
+    );
 }
 
 export default App;
---
+++ todo-fe/src/pages/RegisterPage.js
@@ -0,0 +1,63 @@
+import React, {useState} from "react";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import api from "../utils/api";
+
+const RegisterPage = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [secPassword, setSecpassword] = useState('')
+  const [error, setError] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      if(password !== secPassword) {
+        // 에러 : 패스워드가 일치하지 않습니다 다시 입력해주세요
+        throw new Error('패스워드가 일치하지 않습니다 다시 입력해주세요');
+      }
+      // api
+      const response = await api.post('/user', {name, email, password});
+      navigate('/login')
+    } catch (error) {
+      const message = error.response?.data?.error || error.message || "알 수 없는 에러가 발생했습니다.";
+      setError(message);
+    }
+  };
+  return (
+    <div className="display-center">
+      {error && <div className="red-error">{error}</div>}
+      <Form className="login-box" onSubmit={handleSubmit}>
+        <h1>회원가입</h1>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="string" placeholder="Name" onChange={(event) => setName(event.target.value)} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" onChange={(event) => setEmail(event.target.value)} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>re-enter the password</Form.Label>
+          <Form.Control type="password" placeholder="re-enter the password" onChange={(event) => setSecpassword(event.target.value)} />
+        </Form.Group>
+
+        <Button className="button-primary" type="submit">
+          회원가입
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default RegisterPage;
