5c2f06630b7586a8d352787ff2358c09d955e07c
jihyun1029 <parkji2028@naver.com>
2025-08-21 17:45:39 +0900

    토큰 verify: 토큰으로 유저정보 찾기

---
+++ todo-be/controllers/auth.controller.js
@@ -0,0 +1,25 @@
+const authController = {};
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+// 토큰을 통해서 권한을 가져오기
+authController.authenticate = (req, res, next) => {
+    try {
+        const tokenString = req.headers.authorization; // Bearer <token>
+        if (!tokenString) {
+            throw new Error("invalid token")
+        }
+        const token = tokenString.replace("Bearer ", "");
+        jwt.verify(token, JWT_SECRET_KEY, (error, payload) => {
+            if(error) {
+                throw new Error("invalid token");
+            }
+            console.log("payload??", payload);
+        })
+    } catch (error) {
+        res.status(400).json({status: "fail", message: error.message})
+    }
+}
+
+module.exports = authController
